487c3e72b8facc3b1c9c2ff3d349562264ad0f7d
imad <immaad@gmail.com>
2019-01-26 08:48:46 +1100

    fixed heroku port

--- src/api-local.js
+++ src/api-local.js
@@ -4,10 +4,12 @@ import { SERVICE_NAME } from './configs/constants';
 import moodController from './controllers/mood-controller';
 import LogService from './services/log-service';
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 app.set('storageMock', []);
 app
   .use(LogService.middleware())
   .use(express.json())
   .use([`/${SERVICE_NAME}`, '/'], moodController)
-  .listen(3000, () => console.log('App listening on port 3000!')); // eslint-disable-line no-console
+  .listen(port, () => console.log(`App listening on port ${port}!`)); // eslint-disable-line no-console
